cac35f9fa172ed685dc2d03c27fddd011b722d51
DALEK-PC\Eleanor <eap9238@g.rit.edu>
2018-11-18 18:52:01 -0500

    Update maker.js

--- client/app/maker.js
+++ client/app/maker.js
@@ -24,6 +24,10 @@ const handleDelete = (e) => {
     console.dir(e);
     console.dir("Object Parent 1:");
     console.dir(e.target);
+    console.dir("Object Parent 1 ID:");
+    console.dir(e.target.id);
+    console.dir("Object Parent 1 ID:");
+    console.dir($("#" + e.target.id));
     console.dir("Original: ");
     console.dir($("#deleteDomo"));
     
